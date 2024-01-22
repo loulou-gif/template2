649159ba4a47beff33217d0e04deaef01a72dd9b
unknown <konankanjulius10@gmail.com>
2024-01-22 11:31:53 +0000

    mise en place des routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Header from "./Components/Header"
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route to="/" element={<Header />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
