02599f3de213c0a37a8ecad1b71969e072666201
unknown <konankanjulius10@gmail.com>
2024-01-23 16:41:43 +0000

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import '../assets/Css/Styles.css'
+import { IoMdMenu } from "react-icons/io";
 function Headers() {
     return(
         <header className="header  bg-gray-300">
@@ -10,9 +11,14 @@ function Headers() {
                     <div className=' cercle bg-red-100'></div>
                 </div>
             </div>
-            <nav className='nav-bar flex justify-between p-5 text-3xl'>
-                <h1 className='font-extrabold'>OTAK'UNIVERS</h1>
+            <nav className='nav-bar flex justify-between  w-10/12 mt-10  p-5 text-3xl'>
+                <h1 className='text-white  pl-20 w-5/12 font-extrabold'>OTAK'UNIVERS</h1>
+                <IoMdMenu className='text-white border text-5xl'/>
             </nav>
+            <div className='title border text-white mt-24 w-9/12'>
+                <h1 className='text-3xl font-extrabold'>L'univers des OTAKU vous ouvres ses portes.</h1>
+                <p className='mt-10 text-5xl font-bold w-5/12'>Découvrez l'énergie infinie de la culture animé et manga japonaise. Un monde d'émotions intenses et de créativité débordante vous attend!</p>
+            </div>
         </header>
     )
     
