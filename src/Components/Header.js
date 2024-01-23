1ec72790ae9ce97f5eca59f5cc948c6bc23683fd
unknown <konankanjulius10@gmail.com>
2024-01-23 17:09:00 +0000

    header background

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
 import '../assets/Css/Styles.css'
+// import Banner from '../assets/Img/banner.png'
 import { IoMdMenu } from "react-icons/io";
 function Headers() {
     return(
         <header className="header  bg-gray-300">
-            <div className='social-network flex justify-between bg-white pl-5 -pr-5 border'>
-                <h1 className='text-2xl pt-3 text-red-500 font-extrabold'>OTAK'UNIVERS</h1>
+            <div className='social-network flex justify-between bg-white pl-5 -pr-5 '>
+                <h1 className='text-2xl pt-3 text-white font-extrabold'>OTAK'UNIVERS</h1>
                 <div className='flex justify-end '>
                     <div className='cercle bg-red-100 '></div>
                     <div className='cercle bg-red-100'></div>
@@ -12,10 +13,10 @@ function Headers() {
                 </div>
             </div>
             <nav className='nav-bar flex justify-between  w-10/12 mt-10  p-5 text-3xl'>
-                <h1 className='text-white  pl-20 w-5/12 font-extrabold'>OTAK'UNIVERS</h1>
+                <h1 className='text-white   w-5/12 font-extrabold'>OTAK'UNIVERS</h1>
                 <IoMdMenu className='text-white border text-5xl'/>
             </nav>
-            <div className='title border text-white mt-24 w-9/12'>
+            <div className='title  text-white mt-24 w-9/12'>
                 <h1 className='text-3xl font-extrabold'>L'univers des OTAKU vous ouvres ses portes.</h1>
                 <p className='mt-10 text-5xl font-bold w-5/12'>Découvrez l'énergie infinie de la culture animé et manga japonaise. Un monde d'émotions intenses et de créativité débordante vous attend!</p>
             </div>
