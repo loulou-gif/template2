16b207c9b9db4572a4a818073d8b51c87d1cbd33
unknown <konankanjulius10@gmail.com>
2024-01-23 16:17:15 +0000

    nav-bar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,15 +3,15 @@ function Headers() {
     return(
         <header className="header  bg-gray-300">
             <div className='social-network flex justify-between bg-white pl-5 -pr-5 border'>
-                <h1 className='text-2xl pt-3 text-red-500 font-extrabold'>Titre</h1>
+                <h1 className='text-2xl pt-3 text-red-500 font-extrabold'>OTAK'UNIVERS</h1>
                 <div className='flex justify-end '>
                     <div className='cercle bg-red-100 '></div>
                     <div className='cercle bg-red-100'></div>
                     <div className=' cercle bg-red-100'></div>
                 </div>
             </div>
-            <nav className='nav-bar text-3xl'>
-                <h1>TiTRE</h1>
+            <nav className='nav-bar flex justify-between p-5 text-3xl'>
+                <h1 className='font-extrabold'>OTAK'UNIVERS</h1>
             </nav>
         </header>
     )
