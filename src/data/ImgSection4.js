cf2c02478fdd162985bd8e351063a8ea9feb02ae
unknown <konankanjulius10@gmail.com>
2024-01-24 13:42:54 +0000

    section4

---
+++ src/data/ImgSection4.js
@@ -0,0 +1,22 @@
+import sangoku from '../assets/Img/sangoku.jpg'
+import hunter from '../assets/Img/hunter.jpg'
+import netero from '../assets/Img/netero.jpg'
+import ghoul from '../assets/Img/ghoul.jpg'
+import school from '../assets/Img/sd.jpg'
+import sangoku2 from '../assets/Img/sangoku2.jpg'
+import hunter2 from '../assets/Img/hunter2.jpg'
+
+
+
+const images = {
+    image1: {sangoku},
+    image2: {hunter},
+    image3: {school},
+    image4: {sangoku},
+    image5: {netero},
+    image6: {hunter2},
+    image7: {sangoku2},
+    image8: {ghoul},
+}
+
+export default images;
