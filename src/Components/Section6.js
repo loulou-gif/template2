0dd242b4ef7a6d6cc78f6f6bcf803db90ed4f72b
unknown <konankanjulius10@gmail.com>
2024-01-24 23:11:24 +0000

    modif d'ensemble

--- src/Components/Section6.js
+++ src/Components/Section6.js
@@ -2,9 +2,9 @@ function Section6() {
     return(
         <section className="section6 bg-gray-100">
             <div className="opaque border h-full flex justify-center">
-                <div className="h-full border w-7/12">
-                    <h3 className="text-8xl text-center border font-bold text-white">OTAKU FOREVER</h3>
-                    <p className="text-white text-3xl border ">Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
+                <div className="h-2/4 mt-40 border w-7/12">
+                    <h3 className="text-4xl text-center border font-bold text-white">OTAKU FOREVER</h3>
+                    <p className="text-white text-center text-6xl font-semibold border ">Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
                 </div>
             </div>            
         </section>
