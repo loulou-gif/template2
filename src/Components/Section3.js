80d12bfa6914540ec9690850529616c68b5822bd
unknown <konankanjulius10@gmail.com>
2024-01-24 14:34:32 +0000

    section5 && section1 && 3

--- src/Components/Section3.js
+++ src/Components/Section3.js
@@ -4,7 +4,7 @@ import foodwar from "../assets/Img/fw.png"
 import demonSlayer from "../assets/Img/kny.jpg"
 function Section3() {
     return(
-        <section className="section3 border-b-8 bg-white">
+        <section className="section3 bg-white">
             <div className="flex">
                 <img alt="img-sect3" className="w-1/4" src={naruto} ></img>
                 <img alt="img-sect3" className="w-1/4" src={bleach} ></img>
@@ -13,7 +13,7 @@ function Section3() {
             </div>
             <div className="text-center ">
                 <h3 className="text-2 font-extrabold text-2xl mt-10">Trouvez plus d'idées créatives pour vos projets</h3>
-                <div className="flex justify-center text-3xl mt-5 mb-5 italic">
+                <div className="flex justify-center text-3xl mt-5  italic">
                     <p className="w-5/12 text-gray-500 ">Interface utilisateur belle et facile à comprendre, animations professionnelles et glisser-déposer</p>
                 </div>
             </div>
