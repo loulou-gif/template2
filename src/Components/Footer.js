dc6535e228105c8bfe6786d523a00f88cd5cbb01
unknown <konankanjulius10@gmail.com>
2024-01-25 18:56:15 +0000

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,23 @@
 function Footer() {
     return(
-        <footer className="bg-gray-300">
-            
+        <footer className="bg-gray-300 text-3xl ">
+            <div className="flex justify-evenly item-center mt-10">
+                <div className="">
+                    <h3>GROS TITRE</h3>
+                    <p>De la merdre</p>
+                </div>                        
+                <div className="">
+                    <h3>GROS TITRE</h3>
+                    <p>De la merdre</p>
+                </div>                        
+                <div className="">
+                    <h3>GROS TITRE</h3>
+                    <p>De la merdre</p>
+                </div> 
+            </div>
+            <div className=" h-screen flex justify-center items-center p-10">   
+                <p className="copy text-center text-xl  h-full">Template crée par K4dk27</p>  
+            </div>                     
         </footer>
     )
     
