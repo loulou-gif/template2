d387c4f7ef0788c98f24648015bdbaff536ee8e2
unknown <konankanjulius10@gmail.com>
2024-01-25 18:21:55 +0000

    section8

--- src/Components/Section8.js
+++ src/Components/Section8.js
@@ -1,7 +1,26 @@
+import '../assets/Css/Styles.css'
+
 function Section8() {
     return(
-        <section className="section8 bg-black text-white">
-            
+        <section className="section8 flex p-10 bg-black text-white">
+            <div className="w-6/12 h-full  flex justify-center items-center">
+                <div className='  text-3xl p-5'>
+                    <p className='text-gray-300 font-semibold border-b-4 pb-3'>GHJKJHGHNBFGBVBVG</p>
+                    <p className='text-1xl pt-3'>Contact: +225 07 98 39 57 74</p>
+                    <p className='text-1xl pt-3'>TEXT: dolor sit amet, consectetur adipiscing</p>
+                </div>
+            </div>            
+            <div className="w-6/12 h-full flex justify-center items-center  ">
+                <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.31823678591!2d-3.9558710259081797!3d5.368339835510069!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfc1eb687564e241%3A0xad68bfef48b8dcf3!2zU2ltcGxvbiBDw7R0ZSBk4oCZSXZvaXJl!5e0!3m2!1sfr!2sci!4v1706205425367!5m2!1sfr!2sci" 
+                    allowfullscreen="" 
+                    width="800"
+                    height="600"
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade" 
+                    style={{border:0}}
+                    >
+                </iframe>
+            </div>             
         </section>
     )
     
