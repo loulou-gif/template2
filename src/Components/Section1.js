851594161b9031763489609235d884478f8950b7
unknown <konankanjulius10@gmail.com>
2024-01-26 14:57:29 +0000

    section1 responsive

--- src/Components/Section1.js
+++ src/Components/Section1.js
@@ -5,25 +5,25 @@ import luffy from '../assets/Img/luffy.jpg'
 import ken from '../assets/Img/ghoul.jpg'
 function Section1() {
     return(
-        <section className="section1 flex justify-center">
-            <div className="box border w-2/12  rounded-md bg-white ml-10 -mt-52 ">
+        <section className="section1 flex border justify-center">
+            <div className="box border   rounded-md bg-white ml-10 -mt-52 ">
                 <div className='flex justify-center mt-10'>
                     <img alt="naruto" className="second-cercle   " src={naruto} /> 
                 </div>
                 <h3 className='text-3xl mt-14 font-bold w-full text-center'>Naruto</h3>
                 <p className='w-full  text-center mt-10  text-xl italic text-gray-400 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
             </div>
-            <div className="box border w-2/12  rounded-md bg-white ml-20 -mt-52">
+            <div className="box border   rounded-md bg-white ml-10 lg:ml-20 mt-10 lg:-mt-52">
                 <div className='flex justify-center mt-10'><img alt="naruto" className="second-cercle     " src={luffy} /></div>
                 <h3 className='text-3xl mt-14 font-bold w-full text-center'>Luffy</h3>
                 <p className='w-full  text-center mt-10 text-xl italic text-gray-400 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
             </div>
-            <div className="box border w-2/12  rounded-md bg-white ml-20 -mt-52">
+            <div className="box border   rounded-md bg-white ml-10 lg:ml-20 mt-10 lg:-mt-52">
                 <div className='flex justify-center mt-10'><img alt="naruto" className="second-cercle     " src={natsu} /></div>
                 <h3 className='text-3xl mt-14 font-bold w-full text-center'>Natsu</h3>
                 <p className='w-full  text-center mt-10 text-xl italic text-gray-400 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
             </div>
-            <div className="box border w-2/12  rounded-md bg-white ml-20 -mt-52">
+            <div className="box border   rounded-md bg-white ml-10 lg:ml-20 mt-10 lg:-mt-52">
                 <div className='flex justify-center mt-10'><img alt="naruto" className="second-cercle    " src={ken} /></div>
                 <h3 className='text-3xl mt-14 font-bold w-full text-center'>Kaneki-ken</h3>
                 <p className='w-full  text-center mt-10 text-xl italic text-gray-400 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
