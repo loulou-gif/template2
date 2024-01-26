786c1fb48da27c6fc9d18b893b59ef47017c568d
unknown <konankanjulius10@gmail.com>
2024-01-26 16:57:46 +0000

    header responsive1

--- src/Components/Section2.js
+++ src/Components/Section2.js
@@ -4,20 +4,20 @@ import pull1 from '../assets/Img/pull1.jpg'
 function Section2() {
     return(
         <section className="section2 bg-red-50 flex ">
-            <img alt="anime" className="w-full bg-gray-200" src={pull1} />          
-            <div className="w-8/12 m-20  mt-40">
-                <div className="min-box p-5 ">
-                    <h3 className='text-3xl mt-20 font-bold w-full'>Naruto</h3>
-                    <p className='w-full  mt-10 text-xl italic text-gray-400 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
+            <img alt="anime" className="anime lg:w-full bg-gray-500" src={pull1} />          
+            <div className="complete lg:w-8/12 w-full bg-wh border  lg:mt-40">
+                <div className="mini-box mt-20 border p-5 ">
+                    <h3 className='text-3xl  lg:mt-20 mt-5 border font-bold w-full'>Naruto</h3>
+                    <p className='w-full  mt-5  italic text-gray-500 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices elit nullam nunc justo sagittis suscipit ultrices elit nullam nunc justo sagittis suscipit ultrices</p>
                 </div>
-                <div className="min-box flex  h-2/4  ">
-                    <div className="w-6/12 mt-10   p-5">                    
-                        <h3 className='text-3xl mt-20 font-bold w-full'>Naruto</h3>
-                        <p className='w-full  mt-10  text-xl italic text-gray-400 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
+                <div className=" lg:flex mg:flex  h-1/4  ">
+                    <div className="inside lg:w-6/12 ml:w-6/12  lg:mt-10   p-5">                    
+                        <h3 className='text-3xl mt-5 font-bold border w-full'>Naruto</h3>
+                        <p className='w-full  mt-10   italic text-gray-500 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
                     </div>     
-                    <div className="w-6/12 mt-10 p-5">
-                        <h3 className='text-3xl mt-20 font-bold w-full'>Naruto</h3>
-                        <p className='w-full  mt-10  text-xl italic text-gray-400 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
+                    <div className="inside  lg:w-6/12 ml:w-6/12 border  lg:mt-10 p-5">
+                        <h3 className='text-3xl mt-5 font-bold w-full'>Naruto</h3>
+                        <p className='w-full  mt-10   italic text-gray-500 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
                     </div>     
                 </div>  
             </div>          
