b76e02dccdc6f8323d260452f2d24531c5fdbca0
unknown <konankanjulius10@gmail.com>
2024-01-30 13:10:49 +0000

    global

--- src/Components/Section2.js
+++ src/Components/Section2.js
@@ -5,17 +5,17 @@ function Section2() {
     return(
         <section className="section2 bg-red-50 flex  ">
             <img alt="anime" className="anime lg:w-6/12 bg-gray-500" src={pull1} />          
-            <div className="complete lg:w-8/12 w-full bg-wh border  lg:mt-40">
-                <div className="mini-box mt-20 md:w-6/12   p-5 ">
+            <div className="complete lg:w-8/12 w-full bg-wh border md:flex md:justify-center md:flex-col md:items-center ">
+                <div className="mini-box  md:w-6/12   p-5 ">
                     <h3 className='text-3xl  lg:mt-20 mt-5  font-bold w-full'>Naruto</h3>
                     <p className='w-full  mt-5  italic text-gray-500 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices elit nullam nunc justo sagittis suscipit ultrices elit nullam nunc justo sagittis suscipit ultrices</p>
                 </div>
-                <div className=" lg:flex h-1/4   ">
-                    <div className="inside lg:w-6/12 md:w-6/12  lg:mt-10   p-5">                    
+                <div className=" lg:flex h-1/4  md:w-6/12  ">
+                    <div className="inside lg:w-6/12 w-full lg:mt-10   p-5">                    
                         <h3 className='text-3xl mt-5 font-bold  w-full'>Naruto</h3>
                         <p className='w-full  mt-10   italic text-gray-500 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
                     </div>     
-                    <div className="inside  lg:w-6/12 md:w-6/12   lg:mt-10 p-5">
+                    <div className="inside  lg:w-6/12 w-full  lg:mt-10 p-5">
                         <h3 className='text-3xl mt-5 font-bold w-full'>Naruto</h3>
                         <p className='w-full  mt-10   italic text-gray-500 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices</p>
                     </div>     
